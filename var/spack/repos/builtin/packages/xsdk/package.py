cde1deea63f23f7ff8e175eeebee809533938c6a
Satish Balay <balay@mcs.anl.gov>
2019-10-14 19:26:02 -0500

    revert amrex to 19.08

--- var/spack/repos/builtin/packages/xsdk/package.py
+++ var/spack/repos/builtin/packages/xsdk/package.py
@@ -112,8 +112,8 @@ class Xsdk(BundlePackage):
 
     depends_on('amrex@develop+sundials', when='@develop %intel')
     depends_on('amrex@develop+sundials', when='@develop %gcc')
-    depends_on('amrex@19.10+sundials', when='@0.5.0 %intel')
-    depends_on('amrex@19.10+sundials', when='@0.5.0 %gcc')
+    depends_on('amrex@19.08+sundials', when='@0.5.0 %intel')
+    depends_on('amrex@19.08+sundials', when='@0.5.0 %gcc')
     depends_on('amrex@18.10.1', when='@0.4.0 %intel')
     depends_on('amrex@18.10.1', when='@0.4.0 %gcc')
 
