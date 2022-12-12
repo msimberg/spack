5fa45be7bb0421f9ebc065e8cce32bf6ea12d31c
Mikael Simberg <simberg@cscs.ch>
2022-12-12 16:51:45 +0100

    Add patch for hip package

--- var/spack/repos/builtin/packages/hip/package.py
+++ var/spack/repos/builtin/packages/hip/package.py
@@ -276,6 +276,7 @@ class Hip(CMakePackage):
         patch("0005-Disable-tests-4.1.0.patch", when="@4.1.0:4.3.2")
 
     patch("Add_missing_open_cl_header_file_for_4.3.0.patch", when="@4.3.0:4.3.2")
+    patch("add_missing_quotes_hipamd.patch")
 
     @property
     def root_cmakelists_dir(self):
