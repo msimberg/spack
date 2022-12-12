ee9857d57858888cf3b400c6781ec9968dc19cc2
Mikael Simberg <simberg@cscs.ch>
2022-12-12 16:52:11 +0100

    Change pika default allocator to mimalloc

--- var/spack/repos/builtin/packages/pika/package.py
+++ var/spack/repos/builtin/packages/pika/package.py
@@ -43,7 +43,7 @@ class Pika(CMakePackage, CudaPackage, ROCmPackage):
 
     variant(
         "malloc",
-        default="tcmalloc",
+        default="mimalloc",
         description="Define which allocator will be linked in",
         values=("system", "jemalloc", "mimalloc", "tbbmalloc", "tcmalloc"),
     )
